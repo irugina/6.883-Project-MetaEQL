38e44c33bc4a7e7f0b9d9d8fdaf5eb357ee1b1ab
ileana <irugina@mit.edu>
2020-10-16 15:35:51 -0400

    skeleton code for meta-learning setup

--- feynman_ai_equations.py
+++ feynman_ai_equations.py
@@ -1,5 +1,6 @@
 import numpy as np 
 equation_dict = {
+        "id": lambda x: x, #this is not from feynman ai
         "exp1": lambda x: np.exp(-x**2/2)/np.sqrt(2*np.pi),
         "exp2": lambda x, sigma: np.exp(-(x/sigma)**2/2)/(np.sqrt(2*np.pi)*sigma),
         "exp3": lambda x, x_0, sigma: np.exp(-((x-x_0)/sigma)**2/2)/(np.sqrt(2*np.pi)*sigma),
